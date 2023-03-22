2b317f4ee16341dcff621fca604951dfb9083afe
ngocanhisme <125735821+ngocanhisme@users.noreply.github.com>
2023-03-21 23:23:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ function colorChange(id) {
     colorChange3();
     colorChange1(id);
 }
+function colorChangee(id) {
+    colorChange4();
+    colorChange1(id);
+}
 function colorChange3() {
     let id1 = 'aa';
     let id2 = 'bb';
     let id3 = 'cc'; 
+    let idList = [id1, id2, id3];
+    idList.forEach(function (ID) {
+        colorChange2(ID);
+    });
+}
+function colorChange4() {
+    let id1 = 'aa1';
+    let id2 = 'bb1';
+    let id3 = 'cc1'; 
     let idList = [id1, id2, id3];
     idList.forEach(function (ID) {
         colorChange2(ID);
